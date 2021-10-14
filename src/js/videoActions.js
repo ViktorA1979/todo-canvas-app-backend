91916feb5ac548626a052defa332cc0c6be53fc4
1b0b70ed7fc4ccdf214d1704cef0d01c5cc48d5581fdd1b67c5f087680bcc7808f0f4aa2e0a59461539be92700426298 (JAICP) <1b0b70ed7fc4ccdf214d1704cef0d01c5cc48d5581fdd1b67c5f087680bcc7808f0f4aa2e0a59461539be92700426298>
2021-10-14 13:36:17 +0000

    Edited files: /src/js/findItem.js /src/js/getters.js /src/js/reply.js /src/scenario/addNote.sc /src/scenario/deleteNote.sc /src/scenario/doNote.sc /src/scenario/serverActions.sc /src/js/videoActions.js

--- src/js/videoActions.js
+++ src/js/videoActions.js
@@ -1,7 +1,19 @@
 // "udovolstvie" - id эмоции
 function starnVideo(note, context) {
-    addAction({
-        type: "start_video",
-        note: note,
-    }, context, "udovolstvie");
+    
+    $response.replies = $response.replies || [];
+    
+    var body = {
+          items: [{
+            command :{
+                        type:"add_note",
+                        note $parseTree._anyText
+                     }
+                
+                  }]
+                };
+    
+    $response.replies.push({ type : "raw", body : body });
+
+    
 }
